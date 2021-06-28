effa7948e036f383f3eb905a7ae6c3da35c9298b
vanthinh97 <thinhdangvan97@gmail.com>
2021-06-28 20:43:22 +0700

    End of section 9

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -52,7 +52,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users
-                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName);
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.UserName.ToLower());
 
             if (user == null) return Unauthorized("Invalid username");
 
---
+++ client/src/app/_services/members.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Member } from '../_models/member';
+
+// const httpOptions = new HttpHeaders({
+//     Authorization: 'Bearer' + JSON.parse(localStorage.getItem('user') + '').token
+//   })
+// const token = JSON.parse(localStorage.getItem('user') + '') != null ? 
+//           JSON.parse(localStorage.getItem('user') + '').token : null
+// const httpOptions = {
+//   headers: new HttpHeaders({
+//     Authorization : 'Bearer ' + token
+//   })
+// }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MembersService {
+
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getMembers() {
+    return this.http.get<Member[]>(this.baseUrl + 'users')
+  }
+
+  getMember(username : string) {
+    return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+}
